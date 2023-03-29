d7294a320f389cc549f37c1dbbbbac112dea7805
mkumar-02 <mkumar6@ch.iitr.ac.in>
2023-03-29 18:57:02 +0530

    code cleanup

--- g2p_proxy_means_test/models/proxy_means_params.py
+++ g2p_proxy_means_test/models/proxy_means_params.py
@@ -5,8 +5,8 @@ from odoo import api, fields, models
 class G2PProgram(models.Model):
     _inherit = "g2p.program"
 
-    pmt_config = fields.Boolean(string="Enable Proxy Means Test")
-    proxy_means_params_id = fields.One2many("g2p.proxy_means_test_params", "program_id", string="PMT Parameters")
+    pmt_config = fields.Boolean(string="Enable PMT")
+    proxy_means_params_ids = fields.One2many("g2p.proxy_means_test_params", "program_id", string="PMT Parameters")
     
 class ProxyMeanTestParams(models.Model):
 
@@ -14,21 +14,24 @@ class ProxyMeanTestParams(models.Model):
     _description = "Proxy Means Test Params"
 
     program_id = fields.Many2one("g2p.program", default= lambda self: self.env.uid)
-    pmt_field = fields.Selection(selection="_get_fields_label", string="Field")
+    pmt_field = fields.Selection(selection="get_fields_label", string="Field")
     pmt_weightage = fields.Float(string="Weightage")  
 
-    def _get_fields_label(self):
-        data = self.env["g2p.program_membership"]
-        
-        print("============= test =============")
+    def get_fields_label(self):
+        data = self.env["g2p.program.registrant_info"]
+
+        ir_model_obj = self.env["ir.model.fields"]
         
         choice = []
         for field in data.fields_get_keys():
-            choice.append((field, field))
+            ir_model_field = ir_model_obj.search([('model', '=', "g2p.program.registrant_info"), ('name', '=', field)])
+            field_type = ir_model_field.ttype
+            if field_type == 'integer':
+                choice.append((field, field))
         return choice
         
-class G2PProgramMembership(models.Model):
-    _inherit = "g2p.program_membership"
+class G2PProgramRegistrantInfo(models.Model):
+    _inherit = "g2p.program.registrant_info"
 
     pmt_score = fields.Integer(
         "PMT Score", compute="_compute_pmt_score", store=True
@@ -38,8 +41,14 @@ class G2PProgramMembership(models.Model):
     def _compute_pmt_score(self):
         data = self.env["g2p.proxy_means_test_params"].search([("program_id", "=", self.program_id.id)])
         score = 0
-        for rec in data:
-            score += rec.pmt_weightage 
 
+        
+        print("=============== test ===================")
+        for rec in data:
+            # field_name = rec.pmt_field
+            # field_value = self.env["g2p.program.registrant_info"].search([("program_id", "=", rec.program_id.id)])
+            # print(field_value._context())
+            score += rec.pmt_weightage
+            
         self.pmt_score = score
 
